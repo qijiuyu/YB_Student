95ed52fbbf6bbb96a77a61e4615418d962c5e12b
qijiuyu79 <qijiuyu@bjgoodwill.com>
2020-08-15 21:23:33 +0800

    修改

---
+++ app/src/main/java/com/ylean/yb/student/activity/user/bank/ApplyRefundActivity.java
@@ -0,0 +1,120 @@
+package com.ylean.yb.student.activity.user.bank;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.entity.LocalMedia;
+import com.ylean.yb.student.R;
+import com.ylean.yb.student.activity.declare.ApplySuccessActivity;
+import com.ylean.yb.student.base.BaseActivity;
+import com.ylean.yb.student.utils.SelectPhotoUtil;
+import java.io.File;
+import java.util.List;
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * 申请退还奖学金
+ */
+public class ApplyRefundActivity extends BaseActivity {
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.tv_name)
+    TextView tvName;
+    @BindView(R.id.tv_school)
+    TextView tvSchool;
+    @BindView(R.id.tv_bank_code)
+    TextView tvBankCode;
+    @BindView(R.id.tv_send_des)
+    TextView tvSendDes;
+    @BindView(R.id.tv_send_time)
+    TextView tvSendTime;
+    @BindView(R.id.tv_money)
+    TextView tvMoney;
+    @BindView(R.id.img_bank)
+    ImageView imgBank;
+    @BindView(R.id.img_apply)
+    ImageView imgApply;
+    @BindView(R.id.et_content)
+    EditText etContent;
+    //图片类型
+    private int imgType;
+
+    /**
+     * 加载布局
+     * @return
+     */
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_apply_refund;
+    }
+
+
+    /**
+     * 初始化
+     */
+    @Override
+    protected void initData() {
+        super.initData();
+        tvTitle.setText("申请退还奖学金");
+    }
+
+    @OnClick({R.id.lin_back, R.id.img_bank, R.id.img_apply, R.id.img_template, R.id.tv_submit})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.lin_back:
+                 finish();
+                break;
+            case R.id.img_bank:
+                imgType=1;
+                SelectPhotoUtil.SelectPhoto(this,1);
+                break;
+            case R.id.img_apply:
+                imgType=2;
+                SelectPhotoUtil.SelectPhoto(this,1);
+                break;
+            case R.id.img_template:
+                break;
+            case R.id.tv_submit:
+                setClass(ApplySuccessActivity.class);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            //返回拍照图片
+            case SelectPhotoUtil.CODE_CAMERA_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    File tempFile = new File(SelectPhotoUtil.pai);
+                    if(imgType==1){
+                        Glide.with(this).load(tempFile).into(imgBank);
+                    }else {
+                        Glide.with(this).load(tempFile).into(imgApply);
+                    }
+                }
+                break;
+            //返回相册选择图片
+            case PictureConfig.CHOOSE_REQUEST:
+                List<LocalMedia> list= PictureSelector.obtainMultipleResult(data);
+                if(imgType==1){
+                    Glide.with(this).load(list.get(0).getCompressPath()).into(imgBank);
+                }else {
+                    Glide.with(this).load(list.get(0).getCompressPath()).into(imgApply);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
--- app/src/main/java/com/ylean/yb/student/activity/user/bank/MoneyIssueActivity.java
+++ app/src/main/java/com/ylean/yb/student/activity/user/bank/MoneyIssueActivity.java
@@ -1,18 +1,14 @@
 package com.ylean.yb.student.activity.user.bank;
 
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import com.ylean.yb.student.R;
 import com.ylean.yb.student.adapter.user.bank.MoneyIssueAdapter;
 import com.ylean.yb.student.base.BaseActivity;
 import com.zxdc.utils.library.view.refresh.MyRefreshLayout;
-
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -48,11 +44,47 @@ public class MoneyIssueActivity extends BaseActivity {
         super.initData();
         tvTitle.setText("资金发放明细");
 
+        //展示头部view
+        showHead();
+
+        //初始化列表
+        listView.setDivider(null);
+        listView.setAdapter(adapter=new MoneyIssueAdapter(this));
+    }
+
+
+    /**
+     * 展示头部view
+     */
+    private void showHead(){
         //获取头部view
         headView= LayoutInflater.from(this).inflate(R.layout.head_money_issue,null);
-        listView.setDivider(null);
         listView.addHeaderView(headView);
-        listView.setAdapter(adapter=new MoneyIssueAdapter(this));
+        final TextView tvTitle=headView.findViewById(R.id.tv_title);
+        final TextView tvStatus=headView.findViewById(R.id.tv_status);
+        final TextView tvTime=headView.findViewById(R.id.tv_time);
+        final TextView tvTotalMoney=headView.findViewById(R.id.tv_total_money);
+        final TextView tvSendMoney=headView.findViewById(R.id.tv_send_money);
+        final TextView tvSendNum=headView.findViewById(R.id.tv_send_num);
+        final TextView tvGetMoney=headView.findViewById(R.id.tv_get_money);
+        final TextView tvGetNum=headView.findViewById(R.id.tv_get_num);
+        final TextView tvReissueNum=headView.findViewById(R.id.tv_reissue_num);
+        final TextView tvApplyReissue=headView.findViewById(R.id.tv_apply_reissue);
+        final TextView tvApplyRefund=headView.findViewById(R.id.tv_apply_refund);
+        //补发申请
+        tvApplyReissue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setClass(ApplyReissueActivity.class);
+            }
+        });
+        //申请退还金额
+        tvApplyRefund.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setClass(ApplyRefundActivity.class);
+            }
+        });
     }
 
     @OnClick(R.id.lin_back)
